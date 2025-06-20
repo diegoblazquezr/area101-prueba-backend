8c05156c2133373e527dc242799b785dda778d36
diegoblazquezr <diegoblazquezr@gmail.com>
2025-06-20 19:37:05 +0200

    app name conflict solved

--- community_sessions/apps.py
+++ community_sessions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ComunidadConfig(AppConfig):
+class CommunitySessionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "comunidad"
+    name = "community_sessions"
