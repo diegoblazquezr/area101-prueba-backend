890813a5282479bb0203e9e01ec4a4b557061bc5
diegoblazquezr <diegoblazquezr@gmail.com>
2025-06-24 17:56:15 +0200

    added pagination with ?page=x

--- area101_backend/settings.py
+++ area101_backend/settings.py
@@ -170,6 +170,8 @@ LOGGING = {
 REST_FRAMEWORK = {
     # Le dice a DRF que use spectacular como generador de esquemas
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Número de resultados por página
 }
 
 SPECTACULAR_SETTINGS = {
